7c6738297ea45679584b07499f44752822f23aba
mansibansal9648 <mansibansal12345@gmail.com>
2025-01-31 00:10:19 +0530

    no starts with 1

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,13 @@ public class Main {
         System.out.println("Stream API Learning!!");
 
         //1. Given a list of integers, find out all the even numbers that exist in the list using Stream functions?
-        List<Integer> list= Arrays.asList(5,9,14,18,17,26,29,56,47,36);
-        List<Integer> res=list.stream().filter(n->n%2==0).collect(Collectors.toList());
-        System.out.println(res);
+//        List<Integer> list= Arrays.asList(5,9,14,18,17,26,29,56,47,36);
+//        List<Integer> res=list.stream().filter(n->n%2==0).collect(Collectors.toList());
+//        System.out.println(res);
 
+        //2. Given a list of integers, find out all the numbers starting with 1 using Stream functions?
+        List<Integer> list=Arrays.asList(14,18,20,47,11,9,78,10);
+        List<String> res=list.stream().map(s->s+" ").filter(n->n.startsWith("1")).collect(Collectors.toList());
+        System.out.println(res);
     }
 }
